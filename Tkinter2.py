98f27e7b189d18e1f3672a912cebd33e53658644
Sujoy Roy <sujoyroy689@gmail.com>
2018-01-31 17:16:50 +0530

    Update Tkinter2.py

--- Tkinter2.py
+++ Tkinter2.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 
-one = Label(root, text="Label 1", bg="black", fg="white")
+one = Label(root, text="Label 1", bg="black", fg="white") ## Label creates a Label, in this case its created in root which is it's GUI Structure.
 one.pack()
 
 two = Label(root, text="Label 2", bg="red", fg="white")
